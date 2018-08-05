3dcfb2ccc0f7c5790c5ab07d8aafeea4fdc20b08
Sushant <sushantmishra.kiit@gmail.com>
2018-08-05 22:26:13 +0530

    added python example files and code explanation in python-essentials.txt

---
+++ print_using_name.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+def fetch_words():
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for words in line_words:
+                story_words.append(words)
+    			
+    for word in story_words:
+        print(word)
+
+print(__name__)
